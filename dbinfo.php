fb781c9b1fa53ea99eb78b8600838a6e957a2c92
Tommy Thompson <tomthompson828@gmail.com>
2017-02-02 22:11:49 -0500

    editted SQL connection

--- dbinfo.php
+++ dbinfo.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "";
-$dbname = "";
+$host = "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$dbname = "b3tymdazynh0v6zt";
 
-$user = "";
-$pass = "";
+$user = "sgd5q6szk8q8htdx";
+$pass = "mxnz8ud68kk9h4jw";
 
 ?>
